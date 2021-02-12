72200ca7a2ff23fd55bba554f4652eb332930e4a
DarioRoman <darioroman2001@gmail.com>
2021-02-12 13:09:40 -0300

    finish weather exercise.

--- exercicies/weather.py
+++ exercicies/weather.py
@@ -3,9 +3,10 @@
  + What was the maximum temperature in first 10 days of Jan."""
 
 arr = []
+weather_dict = {}
 
-def open_csv(csv):
-    """Handle csv data extraction."""
+def open_csv_to_array(csv):
+    """Extract the data and put it in to array"""
     with open(csv, 'r') as data:
         for line in data:
             ids = line.split(',')
@@ -14,19 +15,27 @@ def open_csv(csv):
                 arr.append(tempeture)
             except:
                 print('')
-    
+
+def open_csv_to_dict(csv):
+    """Extract the data and put it in to dict"""
+    with open(csv, 'r') as data:
+        for line in data:
+            ids = line.split(',')
+            days = ids[0]
+            try:
+                tempeture = int(ids[1])
+                weather_dict[days] = tempeture
+            except:
+                print('')
+                
 
 def get_average_tempeture(days):
     if days > 10 or days < 0:
         raise Exception('The days input are invalid')
     
-    open_csv('nyc_weather.csv')
+    open_csv_to_array('nyc_weather.csv')
 
-    tempetures = 0
-    for i in range(0, days):
-        tempetures += arr[i]
-
-    average_tempeture = tempetures / days
+    average_tempeture = sum(arr[0:7])/len(arr[0:7])
     return print(f'The average tempeture in the first {days} days of jan was {average_tempeture}')
 
 
@@ -34,11 +43,29 @@ def get_max_tempeture(days):
     if days > 10 or days < 0:
         raise Exception('The days input are invalid')
 
-    open_csv('nyc_weather.csv')
+    open_csv_to_array('nyc_weather.csv')
     max_tempeture = max(arr[0:days])
     return print(f'The max tempeture in the first {days} days of jan was {max_tempeture}')
 
+def get_day_tempeture(day):
+    if day > 10 or day < 0:
+        raise Exception('Invalid date')
+    
+    open_csv_to_dict('nyc_weather.csv')
+
+    day_tempeture = weather_dict[f'Jan {day}']
+    return print(f'the tempeture on day {day} was {day_tempeture}')
 
 if __name__ == "__main__":
+    # What was the average temperature in first week of Jan?
     get_average_tempeture(7)
+
+    # What was the maximum temperature in first 10 days of Jan?
     get_max_tempeture(10)
+
+    # What was the temperature on Jan 9?
+    get_day_tempeture(9)
+
+    # What was the temperature on Jan 4
+    get_day_tempeture(4)
+    
