a782555c0f299f354ab2933bfb30a7f1b5f611ef
DarioRoman <darioroman2001@gmail.com>
2021-04-19 11:38:27 -0400

    implements max heap.

---
+++ go/heap.go
@@ -0,0 +1,100 @@
+package main
+
+import "fmt"
+
+// MaxHeap struct hash a slice that holds the array
+type MaxHeap struct {
+	array []int
+}
+
+// Insert adds an element to the heap
+func (h *MaxHeap) Insert(key int) {
+	h.array = append(h.array, key)
+	h.maxHeapifyUp(len(h.array) - 1)
+}
+
+// Extract return the largest key, and remove it from the heap
+func (h *MaxHeap) Extract() int {
+	extracted := h.array[0]
+	l := len(h.array) - 1
+
+	if len(h.array) == 0 {
+		fmt.Printf("the heap is empty")
+		return -1
+	}
+
+	h.array[0] = h.array[l]
+	h.array = h.array[:l]
+
+	h.maxHeapifyDown(0)
+	return extracted
+}
+
+// maxHeapifyUp will heapify from the bottom top
+func (h *MaxHeap) maxHeapifyUp(index int) {
+	for h.array[parent(index)] < h.array[index] {
+		h.swap(parent(index), index)
+		index = parent(index)
+	}
+}
+
+// maxHeapifyDown will heapify top to bottom
+func (h *MaxHeap) maxHeapifyDown(index int) {
+	lastIndex := len(h.array) - 1
+	l, r := left(index), right(index)
+	childToCompare := 0
+
+	for l <= lastIndex {
+		if l == lastIndex {
+			childToCompare = l
+		} else if h.array[l] > h.array[r] {
+			childToCompare = l
+		} else {
+			childToCompare = r
+		}
+
+		if h.array[index] < h.array[childToCompare] {
+			h.swap(index, childToCompare)
+			index = childToCompare
+			l, r = left(index), right(index)
+		} else {
+			return
+		}
+	}
+}
+
+// get the parent index
+func parent(i int) int {
+	return (i - 1) / 2
+}
+
+// get the left child index
+func left(i int) int {
+	return 2*i + 1
+}
+
+// get the rigth child index
+func right(i int) int {
+	return 2*i + 2
+}
+
+// swap keys in the array
+func (h *MaxHeap) swap(i1, i2 int) {
+	h.array[i1], h.array[i2] = h.array[i2], h.array[i1]
+}
+
+func main() {
+	m := &MaxHeap{}
+	fmt.Println(m)
+
+	buildHeap := []int{10, 20, 30, 5, 7, 9, 11, 13, 15, 17}
+	for _, v := range buildHeap {
+		m.Insert(v)
+		fmt.Println(m)
+	}
+
+	for i := 0; i < 5; i++ {
+		m.Extract()
+		fmt.Println(m)
+	}
+}
--- go/trie.go
+++ go/trie.go
@@ -1,1 +1,57 @@
 package main
+
+// AphabetSize is the number of possible characters in the trie
+const AlphabetSize = 26
+
+// Node represents each node in the trie
+type Node struct {
+	children [AlphabetSize]*Node
+	isEnd    bool
+}
+
+// trie represents a trie and has a pointer to the childs
+type Trie struct {
+	root *Node
+}
+
+// InitTrie will create a new Trie
+func InitTrie() *Trie {
+	result := &Trie{root: &Node{}}
+	return result
+}
+
+// Insert will take a word and add it to the trie
+func (t *Trie) Insert(w string) {
+	wordLength := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			currentNode.children[charIndex] = &Node{}
+		}
+
+		currentNode = currentNode.children[charIndex]
+	}
+
+	currentNode.isEnd = true
+}
+
+// Search will take in a word and RETURN true is that word is in the trie
+func (t *Trie) Search(w string) bool {
+	wordLength := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			return false
+		}
+
+		currentNode = currentNode.children[charIndex]
+	}
+
+	if currentNode.isEnd == true {
+		return true
+	}
+
+	return false
+}
